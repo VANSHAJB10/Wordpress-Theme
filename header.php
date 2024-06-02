4577499b32f027e41ab94cfbe558e1c9b1581add
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2024-06-02 18:21:26 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+/*********
+  Headers
+*********/
+?> <!doctype html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset=" <?php bloginfo( 'charset' ) ?> ">
+        <meta name="viewport" content="width=device-width, intial-scale=1">
+
+        <?php wp_head(); ?>
+    </head>
+    <body>
+    
+    </body>
